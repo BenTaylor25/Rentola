d1fe8d7b0c4f34f4ee12c7eedaec0467b8343b8a
Ben Taylor <ben.taylor2573@gmail.com>
2023-11-08 19:58:38 +0000

    ApiController.Problem(List<Error>)

--- Rentola/Controllers/ApiController.cs
+++ Rentola/Controllers/ApiController.cs
@@ -1,9 +1,41 @@
+using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Rentola.Controllers;
 
 [ApiController]
 public class ApiController : ControllerBase
 {
-    // revisit later
+    protected IActionResult Problem(List<Error> errors)
+    {
+        if (errors.All(e => e.Type == ErrorType.Validation))
+        {
+            ModelStateDictionary modelStateDictionary = new();
+
+            foreach (Error error in errors)
+            {
+                modelStateDictionary.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem(modelStateDictionary);
+        }
+
+        if (errors.Any(e => e.Type == ErrorType.Unexpected))
+        {
+            return Problem();
+        }
+
+        Error firstError = errors[0];
+
+        int statusCode = firstError.Type switch
+        {
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: firstError.Description);
+    }
 }
