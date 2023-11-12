c230611253984b7a60f832b2e0aead83b4bfddd4
benlaptop25 <ben.taylor2573@gmail.com>
2023-11-12 11:26:14 +0000

    added other service errors

--- Rentola/ServiceErrors/Errors.Item.cs
+++ Rentola/ServiceErrors/Errors.Item.cs
@@ -6,9 +6,24 @@ public static class Errors
 {
     public static class Item
     {
+        public static Error QuantityNotPositive => Error.Validation(
+            code: "Item.QuantityNotPositive",
+            description: $"Item quantity must be greater than 0."
+        );
+
         public static Error QuantityTooLarge => Error.Validation(
             code: "Item.QuantityTooLarge",
             description: $"Item quantity may not exceed {Models.Item.MAX_QTY.ToString("N0")}."
+        );
+
+        public static Error NameTooShort => Error.Validation(
+            code: "Item.NameTooShort",
+            description: $"Item name length must be at least {Models.Item.MIN_NAME_LEN} character."
+        );
+
+        public static Error NameTooLong => Error.Validation(
+            code: "Item.NameTooLong",
+            description: $"Item name length must be at least {Models.Item.MAX_NAME_LEN} character."
         );
     }
 }
