3780ff4bfaaa70e8b8b1e2cf3dfb5745cbe5adb6
benlaptop25 <ben.taylor2573@gmail.com>
2023-12-16 13:00:39 +0000

    close new item modal if background clicked

--- rentola-ui/src/components/NewItemModal.tsx
+++ rentola-ui/src/components/NewItemModal.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from "react";
 import "./NewItemModal.scss";
 
 interface NewItemModalProps {
@@ -7,11 +8,19 @@ interface NewItemModalProps {
 
 export default function NewItemModal(props: NewItemModalProps) {
   if (!props.isOpen) {
-    return null;
+   return null;
+  }
+
+  function modalClicked(e: MouseEvent<HTMLDivElement>) {
+    const backgroundClicked = (e.target as HTMLElement).id === "new-item-modal-background";
+
+    if (backgroundClicked) {
+        props.close();
+    }
   }
 
   return (
-    <div id="new-item-modal-background" onClick={props.close}>
+    <div id="new-item-modal-background" onClick={modalClicked}>
       <div id="new-item-modal">
         <h1>test</h1>
       </div>
